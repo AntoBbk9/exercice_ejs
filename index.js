2b938c7a3571b2b841cf6a11eb3d6a7de7cb6ff5
Antoinette Belebele <antoinette.belebele@iMac-de-kadea-122.local>
2024-06-07 12:10:14 +0100

    resolve

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
 const express = require('express');
 const path = require("path");
+const app = express();
+const articles = require("./data/db.json")
 app.set("view engine", "ejs");
-app.set("view", __dirname +"/views");
+app.set("views", __dirname + "/views");
 app.use(express.static(__dirname + "/public"))
 
-app.get()
+app.get("/", (req, res) => {
+    res.render("index");
+});
 
-const app = express();
-const port = 8080;
+app.get("/about", (req, res) => {
+    res.render("about");
+});
 
+app.get("/contact", (req, res) => {
+    res.render("contact");
+});
+
+app.get("/articles", (req, res) => {
+    res.render("articles", {articles});
+});
+
+app.get("/articles/:slug", (req, res) => {
+  const {slug} = req.params;
+  const article = articles.find(article => article.slug === slug)
+
+  if(article) {
+    res.render("article", { article });
+  } else {
+    res.render("404");
+  }
+  
+});
+
+app.get("/*", (req, res, next) => {
+    res.render("404");
+});
+
+const port = 3000;
 
 app.listen(port, () => {
     console.log(`Le serveur est lancé ${port}`);
